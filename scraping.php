0c6bec257879e8aeb12259265830022deba3681c
yutokyokutyo <k.yuto.cre@gmail.com>
2015-10-15 19:28:09 +0900

    取得したい結果を表示できるようになった
    まだ中身はめちゃめちゃ

--- scraping.php
+++ scraping.php
@@ -1,9 +1,16 @@
 <?php
 $homepage = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
 $array = explode("\n", $homepage);
+$events = 0;
 foreach ($array as $value) {
     $pos = strpos($value, '<p class="event"');
     if ($pos !== false) {
-        print($value);
+        $hoge = ['<p class="event">', '</p>'];
+        $p_class = str_replace($hoge, "", $value);
+        print(trim($p_class)."\n");
+        $events++;
+        if ($events === 3) {
+            break;
+        }
     }
 }
