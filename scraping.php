8cfa8ff7217301f779c1076a9e1eb9d89412f7c5
yutokyokutyo <k.yuto.cre@gmail.com>
2015-10-22 15:41:38 +0900

    function にまとめた

--- scraping.php
+++ scraping.php
@@ -1,17 +1,21 @@
 <?php
-$html  = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
-$lines = explode("\n", $html);
-$spots = 0;
 
-foreach ($lines as $line) {
-    $line = trim($line);
-    if (strstr($line, '<p class="event"')) {
-        $tags = ['<p class="event">', '</p>'];
-        $spot = str_replace($tags, "", $line);
-        print($spot."\n");
-        $spots++;
-        if ($spots === 3) {
-            break;
+function odekake() {
+    $html  = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
+    $lines = explode("\n", $html);
+    $spots = 0;
+    foreach ($lines as $line) {
+        $line = trim($line);
+        if (strstr($line, '<p class="event"')) {
+            $tags = ['<p class="event">', '</p>'];
+            $spot = str_replace($tags, "", $line);
+            print($spot."\n");
+            $spots++;
+            if ($spots === 3) {
+                break;
+            }
         }
     }
 }
+
+odekake();
