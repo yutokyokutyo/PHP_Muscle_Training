1c79aa0ae2d7873fa785e423373ffbaa62e3e6d2
yutokyokutyo <k.yuto.cre@gmail.com>
2015-10-22 17:12:08 +0900

    テスト項目増やしたり名前変えたり冗長なコードを排除してリファクタリングする

--- scraping.php
+++ scraping.php
@@ -1,10 +1,9 @@
 <?php
 
-function odekake() {
+function odekakeSpots() {
     $html  = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
     $lines = explode("\n", $html);
-    $spots = 0;
-    $hoge = [];
+    $spots = [];
 
     foreach ($lines as $line) {
         $line = trim($line);
@@ -12,24 +11,33 @@ function odekake() {
             $tags = ['<p class="event">', '</p>'];
             $spot = str_replace($tags, "", $line);
             print($spot."\n");
-            array_push($hoge, $spot);
-            $spots++;
-            if ($spots === 3) {
+            array_push($spots, $spot);
+            if (count($spots) === 3) {
                 break;
             }
         }
     }
-    return $hoge;
+    return $spots;
 }
 
 
-function testOdekake() {
-    $spots =  odekake();
+function testOdekakeSpots() {
+    $spots =  odekakeSpots();
     if ($spots[0] === "エプソン アクアパーク品川") {
-        print("Success!");
+        print("Success1!");
     } else {
-        print("Failure!");
+        print("Failure1!");
+    }
+    if ($spots[1] === "東京ソラマチ(R)") {
+        print("Success2!");
+    } else {
+        print("Failure2!");
+    }
+    if ($spots[2] === "J-WORLD TOKYO") {
+        print("Success3!");
+    } else {
+        print("Failure3!");
     }
 }
 
-testOdekake();
+testOdekakeSpots();
