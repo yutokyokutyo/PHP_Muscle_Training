40f0996d9fc9b78ec7792a060814158cd4944278
yutokyokutyo <k.yuto.cre@gmail.com>
2015-10-22 16:37:26 +0900

    テストを追加した
    array_push 筋がついた

--- scraping.php
+++ scraping.php
@@ -4,18 +4,32 @@ function odekake() {
     $html  = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
     $lines = explode("\n", $html);
     $spots = 0;
+    $hoge = [];
+
     foreach ($lines as $line) {
         $line = trim($line);
         if (strstr($line, '<p class="event"')) {
             $tags = ['<p class="event">', '</p>'];
             $spot = str_replace($tags, "", $line);
             print($spot."\n");
+            array_push($hoge, $spot);
             $spots++;
             if ($spots === 3) {
                 break;
             }
         }
     }
+    return $hoge;
 }
 
-odekake();
+
+function testOdekake() {
+    $spots =  odekake();
+    if ($spots[0] === "エプソン アクアパーク品川") {
+        print("Success!");
+    } else {
+        print("Failure!");
+    }
+}
+
+testOdekake();
