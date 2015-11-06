3061a5f883f2a43a40d41225803f659f399a32a8
yutokyokutyo <k.yuto.cre@gmail.com>
2015-11-06 17:51:27 +0900

    粒度高いが仕方ない

--- OdekakeSpots.php
+++ OdekakeSpots.php
@@ -1,9 +1,34 @@
 <?php
 class OdekakeSpots
 {
-    public static function getRanking()
-    {
-        echo "getranking_ok!!";
+    public static function parseHtmlToArray() {
+        $html  = file_get_contents('http://www.walkerplus.com/top/tokyo.html');
+        $lines = explode("\n", $html);
+        return $lines;
+        // ネットが死んでたり、hostサーバが落ちてたらテストが落ちてしまう懸念有り
+    }
+
+    public static function getRanking() {
+        $lines = self::parseHtmlToArray();
+        $spots = [];
+
+        foreach ($lines as $line) {
+            $line = trim($line);
+            if (strstr($line, '<p class="event"')) {
+                $tags = ['<p class="event">', '</p>'];
+                $spot = str_replace($tags, "", $line);
+                array_push($spots, $spot);
+                if (count($spots) === 3) {
+                    break;
+                }
+            }
+        }
+        return $spots;
+    }
+
+    public static function printOdekakeSpots() {
+        $spots = self::getRanking();
+        return implode("\n", $spots);
     }
 }
 
--- OdekakeSpotsTest.php
+++ OdekakeSpotsTest.php
@@ -3,26 +3,48 @@ require('./OdekakeSpots.php');
 
 class OdekakeSpotsTest
 {
-    public function testGetRanking
-    {
-    OdekakeSpots::GetRanking();
+    public static function testParseHtmlToArray() {
+        $lines = OdekakeSpots::parseHtmlToArray();
+        # もしもarrayだったらOk
+        if (is_array($lines)) {
+            print("this is array!!\n");
+        }
+        # もしも配列の中にdoctypeが入ってたらok
+        if (in_array('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">', $lines)) {
+            print("this is html!!\n");
+        }
+    }
 
-    $spots =  odekakeSpots();
-    if ($spots[0] === "エプソン アクアパーク品川") {
-        print("Success1!\n");
-    } else {
-        print("Failure1!\n");
+    public static function testGetRanking()
+    {
+        $spots = OdekakeSpots::getRanking();
+        if ($spots[0] === "エプソン アクアパーク品川") {
+            print("Success1!\n");
+        } else {
+            print("Failure1!\n");
+        }
+        if ($spots[1] === "J-WORLD TOKYO") {
+            print("Success2!\n");
+        } else {
+            print("Failure2!\n");
+        }
+        if ($spots[2] === "三井アウトレットパーク 多摩南大沢") {
+            print("Success3!\n");
+        } else {
+            print("Failure3!\n");
+        }
     }
-    if ($spots[1] === "東京ソラマチ(R)") {
-        print("Success2!\n");
-    } else {
-        print("Failure2!\n");
+
+    public static function testPrintOdekakeSpots()
+    {
+        $spots = OdekakeSpots::printOdekakeSpots();
+        if ($spots === "エプソン アクアパーク品川\nJ-WORLD TOKYO\n三井アウトレットパーク 多摩南大沢") {
+            print("print ok!\n");
+        }
     }
-    if ($spots[2] === "J-WORLD TOKYO") {
-        print("Success3!\n");
-    } else {
-        print("Failure3!\n");
-    }
-    testOdekakeSpots();
 }
-}
+
+OdekakeSpotsTest::testGetRanking();
+OdekakeSpotsTest::testParseHtmlToArray();
+OdekakeSpotsTest::testPrintOdekakeSpots();
+
